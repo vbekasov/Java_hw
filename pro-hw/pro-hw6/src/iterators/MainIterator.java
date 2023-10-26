13b9b497e18852fad233fbcf947ea201a93185f3
vbekasov <vitalijs.bekasovs@gmail.com>
2023-10-26 23:22:04 +0300

    pro hw-6

--- pro-hw/pro-hw6/src/iterators/MainIterator.java
+++ pro-hw/pro-hw6/src/iterators/MainIterator.java
@@ -4,26 +4,45 @@ import java.util.*;
 
 public class MainIterator {
     public static void main(String[] args) {
-        List<Integer> iList = new ArrayList<>();
-        iList.add(3);
-        iList.add(534);
-        iList.add(64);
-        iList.add(27);
-        iList.add(95);
-        iList.add(15);
-        Iterator<Integer> it1 = iList.iterator();
-        while (it1.hasNext())
-            System.out.print(it1.next() + "  ");
+        MyIter it = new MyIter(generateIntArr(6));
 
-        System.out.println("\n============================");
-        Set<String> sSet = new HashSet<>();
-        sSet.add("sdf");
-        sSet.add("khgf");
-        sSet.add("tgb");
-        sSet.add("mkhf");
-        sSet.add("wrty");
-        for (Iterator<String> i = sSet.iterator(); i.hasNext();)
-            System.out.print(i.next() + "  ");
-        System.out.println("");
+        while (it.hasNext())
+            System.out.print(it.next() + "  ");
+
+        System.out.println(" ");
+        System.out.println(it.next());
+    }
+
+    static class MyIter implements Iterator<Integer> {
+        private ArrayList<Integer> inner;
+        private int count = 0;
+
+        public MyIter(ArrayList<Integer> inner) {
+            this.inner = inner;
+            this.inner.sort(Collections.reverseOrder());
+            System.out.println(this.inner);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return count < inner.size();
+        }
+
+        @Override
+        public Integer next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            return inner.get(count++);
+        }
+    }
+
+    private static ArrayList<Integer> generateIntArr(int len) {
+        Random rnd = new Random();
+        ArrayList<Integer> ret = new ArrayList<>(len);
+
+        while (--len >= 0)
+            ret.add(rnd.nextInt(-100, 101));
+
+        return ret;
     }
 }
