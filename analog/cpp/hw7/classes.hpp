1c3954448638bb8bf0e62ffb334f0678d42b734e
vbekasov <vitalijs.bekasovs@gmail.com>
2023-11-08 10:29:28 +0200

    cpp hw7 8.11

--- analog/cpp/hw7/classes.hpp
+++ analog/cpp/hw7/classes.hpp
@@ -10,7 +10,10 @@
 class DictFromCSV
 {
 private:
-    std::unordered_map<char, std::string>   dict;
+    std::unordered_map<char, std::unordered_map<
+        std::string, std::string>>   dict;
+
+    std::pair<std::string, std::string> parseLine(std::string row);
 public:
     DictFromCSV();
     ~DictFromCSV();
@@ -21,27 +24,27 @@ DictFromCSV::DictFromCSV()
 {
     std::string     line;
     std::ifstream   ReadFile(DCSV);
-    std::pair<char, std::string> pairTmp;    
+    std::pair<char, std::string> pairTmp;
+    char        first   = 'A';
+    std::string second  = "Test";
 
-
-    for (char i = 0; i < 5; i++)
+/*
+    for (char i = 0; i < 40; i++)
     {
         getline (ReadFile, line);
         pairTmp = add_line(line);
 
-        if (dict.find(pairTmp.first) == dict.end())
+        if (dict.find(pairTmp.first) == dict.end()) {
             dict.insert(pairTmp);
-            else {
-                dict.insert(pairTmp.first,
-                dict.find(pairTmp.first) + pairTmp.second);
-            }
-            
-
+        } else {
+            dict.at(pairTmp.first) +=  " ||| " + pairTmp.second;
+        }
     }
-
-    for (auto it : dict)
-        std::cout<< it.first << " " <<
-         it.second << std::endl;
+*/
+    for (char i = 0; i < 40; i++)
+        getline (ReadFile, line);
+    
+    std::cout<< line + "\n";
 
     ReadFile.close();    
 }
@@ -52,6 +55,17 @@ DictFromCSV::~DictFromCSV()
 
 std::pair<char, std::string> DictFromCSV::add_line(std::string row) {
     return std::pair<char, std::string>(row[0], &row[1]);
+}
+
+std::pair<std::string, std::string> DictFromCSV::parseLine(std::string row) {
+    char*   word = new char[row.length()];
+    int     pos  = 0;
+
+    for ( ; row[pos] != ','; pos++)
+        word[pos] = row[pos];
+    
+
+    return nullptr;
 }
 
 
